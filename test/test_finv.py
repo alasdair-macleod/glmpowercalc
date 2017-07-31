669e8812821296bff9a5a6c2b55d1d32bbaab454
Qian <Andeeli@users.noreply.github.com>
2017-07-31 14:21:33 -0400

    VER3-165 test_finv added test case for large or negative df2

--- test/test_finv.py
+++ test/test_finv.py
@@ -1,16 +1,26 @@
 from unittest import TestCase
+
 import numpy as np
-from scipy.stats import f
+
 from glmpowercalc.finv import finv
+
 
 class TestFinv(TestCase):
 
-    def test_largedf(self):
+    def test_largedf1(self):
         actual = finv(0.05, 10**8, 1)
         self.assertTrue(np.isnan(actual))
 
-    def test_negativedf(self):
+    def test_largedf2(self):
+        actual = finv(0.05, 1, 10**9.5)
+        self.assertTrue(np.isnan(actual))
+
+    def test_negativedf1(self):
         actual = finv(0.05, -1, 1)
+        self.assertTrue(np.isnan(actual))
+
+    def test_negativedf2(self):
+        actual = finv(0.05, 1, -1)
         self.assertTrue(np.isnan(actual))
 
     def test_finv(self):
