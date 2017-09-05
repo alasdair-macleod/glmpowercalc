9b94fcb649c7e4c0a4c6346124f03e6bcfc5c017
Qian <Andeeli@users.noreply.github.com>
2017-09-05 14:08:11 -0400

    VER3-UNIREP add AS

--- glmpowercalc/unirep.py
+++ glmpowercalc/unirep.py
@@ -2,6 +2,249 @@ import numpy as np
 import sys
 
 
+def AS(irr, lim1, alb, sigma, cc, acc, anc, n, ith, prnt_prob, error_chk):
+    """
+    Computes distribution of a linear combination of non-central chi-
+    squared random variables. Taken from Algorithm AS 155, Applied
+    Statistics (1980), Vol 29, No 3.
+
+    :param irr: Number of chi-squared terms in the sum
+    :param lim1: Maximum number of integration terms
+    :param alb: IRRx1 vector of constant multipliers
+    :param sigma: Coefficient of normal term
+    :param cc: Point at which the distribution function should be evaluated
+    :param acc: Error bound
+    :param anc: Vector of non-centrality parameters
+    :param n: Vector of degrees of freedom
+    :param ith: Vector of ranks of absolute values of ALB
+    :param prnt_prob: =True or = False, True prints QF
+    :param error_chk: =True or =False, True prints ICOUNT, TRACE, IFAULT
+    :return:
+            QF, Probability that the quadratic form is less than CC
+            TRACE, 7x1 vector of variables that indicate the performance
+                    of the procedure
+               TRACE[1] = Absolute value sum
+               TRACE[2] = Total number of integration terms
+               TRACE[3] = Number of integrations
+               TRACE[4] = Integration interval in main integration
+               TRACE[5] = Truncation point in initial integration
+               TRACE[6] = Standard deviation of convergence factor term
+               TRACE[7] = Number of cycles to locate integration parameters
+            IFAULT, Output fault indicator
+                    =0      No error
+                    =1      Requested accuracy could not be obtained
+                    =2      Round-off error possibly significant
+                    =3      Invalid parameters
+                    =4      Unable to location integration parameters
+            ICOUNT, Number of times the function was called
+    """
+    # defines constants
+    aln28 = np.log(2) / 8
+    pi = 2 * np.arccos(0)
+    ndtsrt = True
+    fail = False
+
+    # initialize variables
+    ifault = 0
+    icount = Countr()
+    aintl = 0
+    ersm = 0
+    qf = -1
+    trace = np.zeros(7)
+
+    # produce local copies of some variables
+    c = cc
+    ir = irr
+    lim = lim1
+    acc1 = acc
+
+    # AMEAN, Scalar representing the expected value of Q
+    # SD, Scalar representing the squared deviation of Q- the second moment
+    # ALMAX, Maximum of the constants
+    # ALMIN, Minimum of the constants
+    xlim = lim
+    sigsq = sigma ** 2
+    sd = sigsq
+    almax = 0
+    almin = 0
+    amean = 0
+    j = 1
+
+    while j <= ir:
+        nj = n[j - 1]
+        alj = alb[j - 1]
+        ancj = anc[j - 1]
+        if nj < 0 or ancj < 0:
+            ifault = 3
+            break
+
+        sd = sd + (alj ** 2) * (2 * nj + 4 * ancj)
+        amean = amean + alj * (nj + ancj)
+        if almax >= alj:
+            if almin <= alj:
+                almin = alj
+        else:
+            almax = alj
+        j = j + 1
+
+    if sd == 0:
+        if c == 0:
+            qf = 1
+        else:
+            qf = 0
+    else:
+        if almin == 0 and almax == 0 and sigma == 0:
+            ifault = 3
+        else:
+            sd = np.sqrt(sd)
+            if almax < -almin:
+                almx = -almin
+            else:
+                almx = almax
+
+            # Define starting values for modules FINDU and CTFF;
+            utx = 16 / sd  # In powerlib, it is 16#inv(sd), matrix form
+            up = 4.5 / sd
+            un = -up
+
+            # Calculate the Truncation point without any convergence factor
+            utx = findu(utx,n,alb,anc,0.5 * acc1,lim,icount,sigsq,ir)
+
+            if c != 0 and almx > 0.07 * sd:
+                fail, cfe1 = cfe(n,alb,anc,ith,c,lim,icount,ndtsrt,ir)
+                tausq = 0.25 * acc1 / cfe1
+                if fail:
+                    fail = False
+                else:
+                    if truncn(n, alb, anc, utx, tausq, lim, icount, sigsq, ir) < 0.2 * acc1:
+                        sigsq = sigsq + tausq
+                        utx = findu(utx,n,alb,anc,0.25 * acc1,lim,icount,sigsq,ir)
+                        trace[5] = np.sqrt(tausq)
+            trace[4] = utx
+            acc1 = 0.5 * acc1
+
+            while True:
+                un, fctff = ctff(un,n,alb,anc,acc1,amean,almin,almax,lim,icount,sigsq,ir)
+                d1 = fctff - c
+                if d1 < 0:
+                    qf = 1
+                    break
+                else:
+                    un, fctff = ctff(un,n,alb,anc,acc1,amean,almin,almax,lim,icount,sigsq,ir)
+                    d2 = c - fctff
+                    if d2 < 0:
+                        qf = 0
+                        break
+                    else:
+                        if d1 <= d2:
+                            aintv = d2
+                        else:
+                            aintv = d1
+                        aintv = 2 * (2 * np.arccos(0)) / aintv
+
+                        xnt = utx / aintv
+                        xntm = 3.0 / np.sqrt(acc1)
+                        if xnt <= xntm * 1.5:
+                            break
+                        else:
+                            if xntm > xlim:
+                                ifault = 1
+                                break
+                            else:
+                                ntm = round(xntm, 0)
+                                aintv1 = utx / xntm
+                                x = 2 * (2 * np.arccos(0)) / aintv1
+                                if x <= abs(c):
+                                    break
+                                else:
+                                    fail, cx1 = cfe(n,
+                                                    alb,
+                                                    anc,
+                                                    ith,
+                                                    c - x,
+                                                    lim,
+                                                    icount,
+                                                    ndtsrt,
+                                                    ir)
+                                    fail, cx2 = cfe(n,
+                                                    alb,
+                                                    anc,
+                                                    ith,
+                                                    c + x,
+                                                    lim,
+                                                    icount,
+                                                    ndtsrt,
+                                                    ir)
+                                    tausq = cx1 + cx2
+                                    tausq = 0.33 * acc1 / (1.1 * tausq)
+                                    if fail:
+                                        break
+                                    else:
+                                        acc1 = 0.67 * acc1
+                                        aintl, ersm = integr(n,
+                                                             alb,
+                                                             anc,
+                                                             ntm,
+                                                             aintv1,
+                                                             tausq,
+                                                             False,
+                                                             c,
+                                                             sigsq,
+                                                             ir)
+                                        xlim = xlim - xntm
+                                        sigsq = sigsq + tausq
+                                        trace[2] = trace[2] + 1
+                                        trace[1] = trace[1] + ntm + 1
+                                        utx = findu(utx,
+                                                    n,
+                                                    alb,
+                                                    anc,
+                                                    0.25 * acc1,
+                                                    lim,
+                                                    icount,
+                                                    sigsq,
+                                                    ir)
+                                        acc1 = 0.75 * acc1
+
+                        trace[3] = aintv
+                        if xnt > xlim:
+                            ifault = 1
+                        else:
+                            nt = round(xnt, 0)
+                            aintl, ersm = integr(n,
+                                                 alb,
+                                                 anc,
+                                                 nt,
+                                                 aintv,
+                                                 0,
+                                                 True,
+                                                 c,
+                                                 sigsq,
+                                                 ir)
+                            trace[2] = trace[2] + 1
+                            trace[1] = trace[1] + nt + 1
+                            qf = 0.5 - aintl
+                            trace[0] = ersm
+                            up = ersm
+
+                            x = up + (acc / 10.0)
+                            j = 1
+
+                            while j <= 8:
+                                if j * x == j * up:
+                                    ifault = 2
+                                j = j + 2
+
+                            trace[6] = icount.count
+
+    if prnt_prob:
+        print(qf)
+    if error_chk:
+        print(trace, ifault, icount.count)
+
+    return qf, trace, ifault, icount.count
+
+
 class Countr(object):
     """ object to hold number of calls of Countr"""
 
