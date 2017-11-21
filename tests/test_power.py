aa08b8964a08ede94f191510b430115448169652
Qian <Andeeli@users.noreply.github.com>
2017-11-21 14:17:47 -0500

    VER3-power fixed test_unirep

--- tests/test_power.py
+++ tests/test_power.py
@@ -1,2 +1,8 @@
+from unittest import TestCase
+import numpy as np
+from glmpowercalc import unirep
+from glmpowercalc.constants import Constants
 
 class TestPower(TestCase):
+    def TestPower(self):
+        pass
