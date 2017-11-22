b5fb4fbe602e8f61764936519c91593377124900
Qian <Andeeli@users.noreply.github.com>
2017-11-22 15:54:02 -0500

    VER3-power testing Power

--- tests/test_power.py
+++ tests/test_power.py
@@ -2,11 +2,11 @@ from unittest import TestCase
 import numpy as np
 from glmpowercalc import unirep
 from glmpowercalc.constants import Constants
-from glmpowercalc.power import Power
+from glmpowercalc.power import CL, IP, Power
 
 
 class TestPower(TestCase):
-    def TestPower(self):
+    def test_Power(self):
         expected = 0.9
-        actual = Power().power
-        self.assertEqual(expected, actual)
+        actual = Power
+        self.assertEqual(expected, actual.power(self))
