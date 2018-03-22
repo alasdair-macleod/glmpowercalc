d856082df623dc8222f847ec323d96c7e27eb6f1
Qian <Andeeli@users.noreply.github.com>
2018-03-22 14:04:15 -0400

    VER3-291 orpol testing

--- glmpowercalc/orpol.py
+++ glmpowercalc/orpol.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def orpol(x, maxdegree, weights):
+
+def orpol(x, maxdegree=None, weights=None):
     """
     The orpol function generates orthogonal polynomials on a discrete set of points.
     Reference: Emerson 1968
@@ -13,27 +14,23 @@ def orpol(x, maxdegree, weights):
     """
 
     # deal with x
-    if x is None:
-        raise Exception('Please enter the vector of values on which the polynomials are to be defined')
-    else:
-        n = np.shape(x)[0]
+    assert x is not None
+    n = np.shape(x)[0]
 
     # deal with the maxdegree
     if maxdegree is None:
         degree_desire = min(n, 19)
-    elif maxdegree > n:
-        raise Exception('Please use a maxdegree smaller or equal to the number of value in x')
     else:
+        assert maxdegree <= n, 'Please use a maxdegree smaller or equal to the number of value in x'
         degree_desire = min(maxdegree, 19)
 
     # deal with the weights
     if weights is None:
-        weights = np.empty(n)
-    elif np.shape(weights)[0] != n:
-        raise Exception('Please specify weight for each value in x')
+        weights = np.ones(n)
+    assert np.shape(weights)[0] == n, 'Please specify weight for each value in x'
 
     orth_ploy = np.empty((degree_desire+1, n))
-    orth_ploy[0,] = np.zeros(n)
+    orth_ploy[0, ] = np.zeros(n)
     qx = np.ones(n)
 
     for j in range(0, degree_desire):
@@ -42,4 +39,4 @@ def orpol(x, maxdegree, weights):
         B = np.sum(np.multiply(np.multiply(x, weights), np.power(qx, 2)))/np.sum(np.multiply(weights, np.power(qx, 2)))
         qx = np.multiply(x-B, orth_ploy[j+1, ]) - A * orth_ploy[j, ]
 
-    return orth_ploy[1:, ]
+    return orth_ploy[1:, ].T
